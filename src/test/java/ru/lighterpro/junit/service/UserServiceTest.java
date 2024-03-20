9f205568dcbfd5f75b876901f8e18b413f73ff58
LighterPro <LighterPro@gmail.com>
2024-03-21 00:14:34 +0300

    6. Test Driven Development. TDD

--- src/test/java/ru/lighterpro/junit/service/UserServiceTest.java
+++ src/test/java/ru/lighterpro/junit/service/UserServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import ru.lighterpro.junit.dto.User;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,47 +13,67 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 // @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
 
-    UserService userService;
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User OLGA = User.of(2, "Olga", "456");
+
+    private UserService userService;
 
     @BeforeAll
     static void beforeAll() {
-        System.out.println("Before all");
-        System.out.println();
     }
 
     @BeforeEach
     void setUp() {
-        System.out.println("Before each: " + this);
         userService = new UserService();
     }
 
+    // Tests block start //
+
     @Test
     void usersEmptyIfNoUsersAdded() {
-        System.out.println("Before tes1: " + this);
         List<User> users = userService.getAll();
         assertTrue(users.isEmpty(), "Users list should be empty");
     }
 
     @Test
     void userSizeIfUserAddedTest() {
-        System.out.println("Before tes1: " + this);
-
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(OLGA);
 
         List<User> users = userService.getAll();
         assertEquals(2, users.size());
     }
 
+    @Test
+    void loginSuccessIfUserExists() {
+        userService.add(IVAN);
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+        assertTrue(maybeUser.isPresent());
+        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+    }
+
+    @Test
+    void loginFailIfUsernameDoesNotExist() {
+        userService.add(IVAN);
+        Optional<User> maybeUser = userService.login("WrongUsername", IVAN.getPassword());
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfPasswordIncorrect() {
+        userService.add(IVAN);
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), "WrongPassword");
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    // Tests block end //
+
     @AfterEach
     void tearDown() {
-        System.out.println("After each: " + this);
-        System.out.println();
     }
 
     @AfterAll
     static void afterAll() {
-        System.out.println("After all");
-        System.out.println();
     }
 }
